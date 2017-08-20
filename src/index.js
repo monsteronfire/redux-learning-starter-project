38e62d359d5905d72e2b66a213eeea3965e2433c
Shaii Ong <monsteronfire@gmail.com>
2017-08-20 22:59:34 +0800

    Store default starting data in state

--- src/index.js
+++ src/index.js
@@ -2,21 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 import config from '../environment.config';
 
 const API_KEY = config.API_KEY;
 
-YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-  console.log(data);
-});
 
-const App = (props) => {
-  return (
-    <div className='app-wrapper'>
-      <SearchBar/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({ videos: data });
+    });
+  }
+
+  render() {
+    return (
+      <div className='app-wrapper'>
+        <SearchBar/>
+      </div>
+    );
+  };
 };
 
 ReactDOM.render(<App/>, document.querySelector('#container'));
