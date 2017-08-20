a8bb74c0b64a2b03b606a0e65a8c59491eee7692
Shaii Ong <monsteronfire@gmail.com>
2017-08-20 17:31:59 +0800

    Add basic search_bar component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 import config from '../environment.config';
 
 const API_KEY = config.API_KEY;
-console.log(API_KEY);
+
 const App = (props) => {
-  return <div>Hello!</div>;
+  return (
+    <div className='app-wrapper'>
+      <SearchBar/>
+    </div>
+  );
 };
 
 ReactDOM.render(<App/>, document.querySelector('#container'));
