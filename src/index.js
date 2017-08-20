95b271c663e8cde56eae132714fecdc2729fc9d9
Shaii Ong <monsteronfire@gmail.com>
2017-08-20 22:47:38 +0800

    Get sample YouTube data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+
 import config from '../environment.config';
 
 const API_KEY = config.API_KEY;
+
+YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+  console.log(data);
+});
 
 const App = (props) => {
   return (
