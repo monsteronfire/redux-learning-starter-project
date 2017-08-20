fb5ea6530b61acda2b8ccbbda6c92a3e64e00552
Shaii Ong <monsteronfire@gmail.com>
2017-08-20 17:20:03 +0800

    Add initial env config file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import config from '../environment.config';
 
+const API_KEY = config.API_KEY;
+console.log(API_KEY);
 const App = (props) => {
   return <div>Hello!</div>;
 };
