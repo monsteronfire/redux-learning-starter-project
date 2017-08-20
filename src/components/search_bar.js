1639b53379725eb384e60f5f08a806012f92eaf0
Shaii Ong <monsteronfire@gmail.com>
2017-08-20 20:01:27 +0800

    Add handleInputChange for searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,12 +3,25 @@ import React from 'react';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      term: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({ term: event.target.value });
   }
 
   render() {
     return (
-      <input/>
+      <div className='search-bar-wrapper'>
+        <input
+          value={this.state.term}
+          onChange={this.handleInputChange}
+        />
+      </div>
     )
   }
 };
